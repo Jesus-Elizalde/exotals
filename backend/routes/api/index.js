934e8a779ff42e6370f0a4a123b2bd5bca3ea23f
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-03-31 13:46:33 -0700

    signup route complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,7 +5,7 @@ const usersRouter = require("./users.js");
 
 router.use("/session", sessionRouter);
 
-// router.use("/users", usersRouter);
+router.use("/users", usersRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
