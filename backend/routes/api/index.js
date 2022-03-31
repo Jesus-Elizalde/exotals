fab14f41475b431055f00df9e368de5881160721
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-03-31 13:38:11 -0700

    session routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,32 +1,11 @@
 const router = require("express").Router();
 
-const asyncHandler = require("express-async-handler");
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 
-const { restoreUser } = require("../../utils/auth.js");
-const { requireAuth } = require("../../utils/auth.js");
+router.use("/session", sessionRouter);
 
-router.get(
-  "/set-token-cookie",
-  asyncHandler(async (_req, res) => {
-    const user = await User.findOne({
-      where: {
-        username: "Demo-User",
-      },
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-  })
-);
-
-router.get("/restore-user", restoreUser, (req, res) => {
-  return res.json(req.user);
-});
-
-router.get("/require-auth", requireAuth, (req, res) => {
-  return res.json(req.user);
-});
+// router.use("/users", usersRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
