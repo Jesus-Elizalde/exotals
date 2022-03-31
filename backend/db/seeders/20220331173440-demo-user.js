bc6b57f905b890387300e4b04d3743a4646759e2
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-03-31 13:22:48 -0700

    done testing auth middleware

---
+++ backend/db/seeders/20220331173440-demo-user.js
@@ -0,0 +1,39 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "demo@user.io",
+          username: "Demo-User",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "demo2@user.io",
+          username: "Demo-User2",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "demo3@user.io",
+          username: "Demo-User3",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      "Users",
+      {
+        username: { [Op.in]: ["Demo-User", "Demo-User2", "Demo-User3"] },
+      },
+      {}
+    );
+  },
+};
