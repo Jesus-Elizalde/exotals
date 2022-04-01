c88b66dee4595c78f7be1cf685009fb4f96965fb
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-04-01 13:39:11 -0700

    part 2 on auth complete

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupModal from "../SignupFormModal";
+
+// import "./Navigation.css";
+
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = <ProfileButton user={sessionUser} />;
+  } else {
+    sessionLinks = (
+      <>
+        <LoginFormModal />
+        <SignupModal />
+      </>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">
+          Exotals
+        </NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
-
-import "./index.css";
-
 import ReactDOM from "react-dom";
+import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ModalProvider } from "./context/Modal";
 
 import configureStore from "./store";
-
 import { restoreCSRF, csrfFetch } from "./store/csrf";
-
 import * as sessionActions from "./store/session";
 
 const store = configureStore();
@@ -18,17 +15,19 @@ const store = configureStore();
 if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
-  window.sessionActions = sessionActions;
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
