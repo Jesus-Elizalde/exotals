6e3e01120a69a90ae51d0ea744913ffb73b478e3
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-04-03 13:36:06 -0700

    updating image seed file

--- backend/db/seeders/20220402061626-image-data.js
+++ backend/db/seeders/20220402061626-image-data.js
@@ -15,6 +15,22 @@ module.exports = {
           url: "https://images.unsplash.com/photo-1642201855395-1c8b44e6e42b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
           carId: 1,
         },
+        {
+          url: "https://images.unsplash.com/photo-1611859266238-4b98091d9d9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+          carId: 2,
+        },
+        {
+          url: "https://images.unsplash.com/photo-1584273421792-84b448728b38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80",
+          carId: 2,
+        },
+        {
+          url: "https://images.unsplash.com/photo-1598543821338-3b6306e0dcc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+          carId: 2,
+        },
+        {
+          url: "https://images.unsplash.com/photo-1610210572585-248996e150fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
+          carId: 5,
+        },
       ],
       {}
     );
