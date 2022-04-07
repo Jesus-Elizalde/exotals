8049880ab4543f48286ca6fb934faaaaf2c6e3dd
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-04-07 08:56:31 -0700

    review preview v2

---
+++ frontend/src/components/DetailContainer/ReviewMainHolder.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReviewHolder from "./ReviewHolder";
+const ReviewMainHolder = () => {
+  return (
+    <div className="review_main_holder">
+      <ReviewHolder />
+    </div>
+  );
+};
+
+export default ReviewMainHolder;
---
+++ frontend/src/components/DetailContainer/ReviewMainPreview.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+
+const ReviewMainPreview = () => {
+  const { pathname } = useLocation();
+  const carId = pathname.split("/").at(-1);
+  const getAllReviews = useSelector((state) => state.reviews);
+
+  return (
+    <div className="reviewcontainerpreview">
+      {Object.values(getAllReviews)
+        .filter((ele) => ele.carId === +carId)
+        .slice(0, 6)
+        .map((ele, i) => (
+          <div key={i} className="reviewholdersingler">
+            <div className="reviewholdersinglerheader">
+              <h3>{ele.rating}</h3>
+              {/* <div className="revieweditdel">
+                <button>Edit</button>
+                <button>Delete</button>
+              </div> */}
+            </div>
+            <p className="reviewsinglebox">{ele.review}</p>
+            <div>createdat</div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ReviewMainPreview;
