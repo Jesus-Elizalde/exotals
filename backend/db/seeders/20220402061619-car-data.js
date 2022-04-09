8fc6429edfe3a52367fa42c2c07d7bc26b885e6a
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-04-09 15:01:05 -0700

    new data

--- backend/db/seeders/20220402061619-car-data.js
+++ backend/db/seeders/20220402061619-car-data.js
@@ -1,25 +1,41 @@
 "use strict";
 const data = require("../../assests/json/makesandmodelsdb.json");
 const cityData = require("../../assests/json/mega-car_dealers.json");
+
+const cars = require("../../assests/json/cars.json");
 const results = [];
-const colors = ["red", "yellow", "green", "blue", "purple", "grey"];
+// const colors = ["red", "yellow", "green", "blue", "purple", "grey"];
 const price = [2500, 5000, 1000];
 const user = [1, 2, 3, 4];
 
-cityData.forEach((ele) => {
+cars.forEach((ele, i) => {
   results.push({
-    address: ele.address1,
-    city: ele.city,
-    state: ele.state,
-    country: ele.country,
-    description: `During the late 20th and early 21st century cars increased in weight due to batteries,[55] modern steel safety cages, anti-lock brakes, airbags, and "more-powerful—if more-efficient—engines"[56] and, as of 2019, typically weigh between 1 and 3 tonnes.[57] Heavier cars are safer for the driver from a crash perspective, but more dangerous for other vehicles and road users.[56] The weight of a car influences fuel consumption and performance, with more weight resulting in increased fuel consumption and decreased performance. The SmartFortwo, a small city car, weighs 750–795 kg (1,655–1,755 lb). Heavier cars include full-size cars, SUVs and extended-length SUVs like the Suburban.`,
-    price: price[Math.floor(Math.random() * price.length)],
-    color: colors[Math.floor(Math.random() * colors.length)],
+    address: cityData[i].address1,
+    city: cityData[i].city,
+    state: cityData[i].state,
+    country: cityData[i].country,
+    description: ele.description,
+    price: ele.price,
+    color: ele.color,
     userId: user[Math.floor(Math.random() * user.length)],
-    modelId:
-      data.Models[Math.floor(Math.random() * data.Models.length)].modelId + 1,
+    modelId: ele.modelId,
   });
 });
+
+// cityData.forEach((ele) => {
+//   results.push({
+//     address: ele.address1,
+//     city: ele.city,
+//     state: ele.state,
+//     country: ele.country,
+//     description: `During the late 20th and early 21st century cars increased in weight due to batteries,[55] modern steel safety cages, anti-lock brakes, airbags, and "more-powerful—if more-efficient—engines"[56] and, as of 2019, typically weigh between 1 and 3 tonnes.[57] Heavier cars are safer for the driver from a crash perspective, but more dangerous for other vehicles and road users.[56] The weight of a car influences fuel consumption and performance, with more weight resulting in increased fuel consumption and decreased performance. The SmartFortwo, a small city car, weighs 750–795 kg (1,655–1,755 lb). Heavier cars include full-size cars, SUVs and extended-length SUVs like the Suburban.`,
+//     price: price[Math.floor(Math.random() * price.length)],
+//     color: colors[Math.floor(Math.random() * colors.length)],
+//     userId: user[Math.floor(Math.random() * user.length)],
+//     modelId:
+//       data.Models[Math.floor(Math.random() * data.Models.length)].modelId + 1,
+//   });
+// });
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
--- backend/db/seeders/20220402061626-image-data.js
+++ backend/db/seeders/20220402061626-image-data.js
@@ -1,5 +1,20 @@
 "use strict";
+const cars = require("../../assests/json/cars.json");
 
+const results = [];
+
+for (let i = 0; i < cars.length; i++) {
+  for (let j = 0; j < cars[i].images.length; j++) {
+    results.push({
+      url: cars[i].images[j],
+      carId: i + 1,
+    });
+  }
+}
+console.log(
+  "🚀 ~ file: 20220402061626-image-data.js ~ line 5 ~ results",
+  results
+);
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
@@ -8,32 +23,7 @@ module.exports = {
 
       Example:
       */
-    return queryInterface.bulkInsert(
-      "Images",
-      [
-        {
-          url: "https://images.unsplash.com/photo-1642201855395-1c8b44e6e42b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
-          carId: 1,
-        },
-        {
-          url: "https://images.unsplash.com/photo-1611859266238-4b98091d9d9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
-          carId: 2,
-        },
-        {
-          url: "https://images.unsplash.com/photo-1584273421792-84b448728b38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80",
-          carId: 2,
-        },
-        {
-          url: "https://images.unsplash.com/photo-1598543821338-3b6306e0dcc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-          carId: 2,
-        },
-        {
-          url: "https://images.unsplash.com/photo-1610210572585-248996e150fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
-          carId: 5,
-        },
-      ],
-      {}
-    );
+    return queryInterface.bulkInsert("Images", results, {});
   },
 
   down: (queryInterface, Sequelize) => {
